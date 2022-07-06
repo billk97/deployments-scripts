ce57543be64e83d379a8672883fca9808fe5f1d1
billk97 <vasileios@konstantinou.dev>
2022-07-06 12:12:12 +0300

    add chronjob scripts

---
+++ msc-alfa-corp/chronjob.bash
@@ -0,0 +1,11 @@
+previusVersion=$(docker image inspect --format='{{.Id}}' vskgr/controller)
+docker pull vskgr/controller
+newVersion=$(docker image inspect --format='{{.Id}}' vskgr/controller)
+
+if [ "$previusVersion" != "$newVersion" ]; then
+    echo "new version found"
+    # docker-compose stop controller
+    # docker-compose up -d controller
+else
+    echo "no changes"
+fi
